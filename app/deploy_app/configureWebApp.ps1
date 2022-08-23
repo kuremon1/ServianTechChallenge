a614fa211242673fcc986ee792cbaabf1a24820c
unknown <clementchane@gmail.com>
2022-08-23 16:46:29 +0900

    add config commands for web app

--- app/deploy_app/configureWebApp.ps1
+++ app/deploy_app/configureWebApp.ps1
@@ -2,3 +2,13 @@
 $postgresconnectionstring = Get-AzPostgreSqlConnectionString -Name $postgreserver -ResourceGroupName $rgname
 az webapp config connection-string set -g $rgname -n $appname -t PostgresSQLAzure --settings defaultConnection=$postgresconnectionstring
 
+#Add environment variables and retrieve values from key vault
+$dbadmin = Get-AzKeyVaultSecret -VaultName $stckeyvault -Name "dbadmin" -AsPlainText
+az webapp config appsettings set --resource-group $rgname --name $appname --settings dbadmin="$postgresdb.mysql.database.azure.com"
+
+#Enable persistent storage
+az webapp config appsettings set --resource-group $rgname --name $appname --settings WEBSITES_ENABLE_APP_SERVICE_STORAGE=TRUE
+
+#enable health check
+az webapp config set -g $rgname -n $appname --generic-configurations '{"healthCheckPath": "/healthcheck/"}'
+
